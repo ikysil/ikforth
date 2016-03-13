a7fa568fcb67facf2f893e20aef171e15eabcc47
Illya Kysil <ikysil@ikysil.name>
2016-03-13 16:48:09 +0100

    Introduce support for 2VALUE

--- lib/ansi/value.f
+++ lib/ansi/value.f
@@ -1,7 +1,7 @@
 \
 \  value.f
 \
-\  Copyright (C) 1999-2004 Illya Kysil
+\  Copyright (C) 1999-2016 Illya Kysil
 \
 
 CR .( Loading VALUE definitions )
@@ -9,18 +9,82 @@ CR .( Loading VALUE definitions )
 REPORT-NEW-NAME @
 REPORT-NEW-NAME OFF
 
+\ Layout of PFA for VALUE-like words
+\ +0    vt-addr     address of table of methods
+\ +4    data
+\
+\ table of methods layout
+\ +0    store-xt         STATE-sensitive word to perform/compile store operation (!, 2!)
+\ +4    plus-store-xt    STATE-sensitive word to perform/compile plus-store operation (+!, 2+!)
+
+: VALUE>DATA (S body-addr -- data-addr )
+   CELL+
+;
+
+: VALUE-METHOD-DOES
+   DOES> (S value-xt vt-body -- ) \ runtime semantics
+   @
+   SWAP >BODY VALUE>DATA
+   STATE @
+   IF
+      POSTPONE LITERAL
+      COMPILE,
+   ELSE
+      SWAP EXECUTE
+   THEN
+;
+
+: VALUE-METHOD
+  CREATE ' , VALUE-METHOD-DOES
+;
+
+VALUE-METHOD VALUE!   !
+VALUE-METHOD VALUE+!  +!
+VALUE-METHOD 2VALUE!  2!
+VALUE-METHOD 2VALUE+! 2+!
+
+CREATE VALUE-VT
+   ' VALUE! , ' VALUE+! ,
+
+CREATE 2VALUE-VT
+   ' 2VALUE! , ' 2VALUE+! ,
+
+: VALUE>VT (S xt -- int-vt-addr )
+   >BODY @
+;
+
+: VALUE!VT@ (S vt-addr -- store-xt )
+   @
+;
+
+: VALUE+!VT@ (S vt-addr -- plus-store-xt )
+   CELL+ @
+;
+
 \ -----------------------------------------------------------------------------
-\ VALUE TO +TO
+\ VALUE 2VALUE TO +TO
 \ -----------------------------------------------------------------------------
 
-: VALUE CREATE , DOES> @ ;
+: VALUE
+   CREATE
+   VALUE-VT ,
+   ,
+   DOES> VALUE>DATA @
+;
 
-:NONAME ' >BODY ! ;
-:NONAME ' >BODY POSTPONE LITERAL POSTPONE ! ;
-INT/COMP: TO IMMEDIATE
+: 2VALUE
+   CREATE
+   2VALUE-VT ,
+   , ,
+   DOES> VALUE>DATA 2@
+;
 
-:NONAME ' >BODY +! ;
-:NONAME ' >BODY POSTPONE LITERAL POSTPONE +! ;
-INT/COMP: +TO IMMEDIATE
+: TO
+  ' DUP VALUE>VT VALUE!VT@ EXECUTE
+; IMMEDIATE
+
+: +TO
+  ' DUP VALUE>VT VALUE+!VT@ EXECUTE
+; IMMEDIATE
 
 REPORT-NEW-NAME !
--- src/kernel/double.f
+++ src/kernel/double.f
@@ -12,9 +12,9 @@ REPORT-NEW-NAME OFF
 BASE @
 HEX
 
-\ 8.6.1.1050 D- 
+\ 8.6.1.1050 D-
 \ (S d1|ud1 d2|ud2 -- d3|ud3 )
-\ Subtract d2|ud2 from d1|ud1, giving the difference d3|ud3. 
+\ Subtract d2|ud2 from d1|ud1, giving the difference d3|ud3.
 CODE D-
   59 B,                    \ POP     ECX
   5B B,                    \ POP     EBX
@@ -27,9 +27,9 @@ CODE D-
   $NEXT
 END-CODE
 
-\ 8.6.1.1090 D2* 
+\ 8.6.1.1090 D2*
 \ (S xd1 -- xd2 )
-\ xd2 is the result of shifting xd1 one bit toward the most-significant bit, filling the vacated least-significant bit with zero. 
+\ xd2 is the result of shifting xd1 one bit toward the most-significant bit, filling the vacated least-significant bit with zero.
 CODE D2*
   5A B,                    \ POP     EDX
   58 B,                    \ POP     EAX
@@ -38,11 +38,11 @@ CODE D2*
   50 B,                    \ PUSH    EAX
   52 B,                    \ PUSH    EDX
   $NEXT
-END-CODE 
+END-CODE
 
-\ 8.6.1.1100 D2/ 
+\ 8.6.1.1100 D2/
 \ (S xd1 -- xd2 )
-\ xd2 is the result of shifting xd1 one bit toward the least-significant bit, leaving the most-significant bit unchanged. 
+\ xd2 is the result of shifting xd1 one bit toward the least-significant bit, leaving the most-significant bit unchanged.
 CODE D2/
   5A B,                    \ POP     EDX
   58 B,                    \ POP     EAX
@@ -51,11 +51,11 @@ CODE D2/
   50 B,                    \ PUSH    EAX
   52 B,                    \ PUSH    EDX
   $NEXT
-END-CODE 
+END-CODE
 
-\ 8.6.1.1830 M+ 
+\ 8.6.1.1830 M+
 \ (S d1|ud1 n -- d2|ud2 )
-\ Add n to d1|ud1, giving the sum d2|ud2. 
+\ Add n to d1|ud1, giving the sum d2|ud2.
 CODE M+
   5B B,                    \ POP     EBX
   5A B,                    \ POP     EDX
@@ -65,7 +65,7 @@ CODE M+
   50 B,                    \ PUSH    EAX
   52 B,                    \ PUSH    EDX
   $NEXT
-END-CODE 
+END-CODE
 
 BASE !
 
@@ -85,9 +85,9 @@ BASE !
 
 : D= D- D0= ;
 
-\ 8.6.2.1270 DU< 
+\ 8.6.2.1270 DU<
 \ (S ud1 ud2 -- flag )
-\ flag is true if and only if ud1 is less than ud2. 
+\ flag is true if and only if ud1 is less than ud2.
 : DU< (S ud1 ud2 -- flag )
   D- D0<
 ;
@@ -98,23 +98,23 @@ BASE !
   ROT XOR -ROT XOR SWAP
 ;
 
-\ 8.6.1.1220 DMIN 
+\ 8.6.1.1220 DMIN
 \ (S d1 d2 -- d3 )
-\ d3 is the lesser of d1 and d2. 
+\ d3 is the lesser of d1 and d2.
 : DMIN (S d1 d2 -- d3 )
   2OVER 2OVER D> IF 2SWAP THEN 2DROP
 ;
 
-\ 8.6.1.1210 DMAX 
+\ 8.6.1.1210 DMAX
 \ (S d1 d2 -- d3 )
-\ d3 is the greater of d1 and d2. 
+\ d3 is the greater of d1 and d2.
 : DMAX (S d1 d2 -- d3 )
   2OVER 2OVER D< IF 2SWAP THEN 2DROP
 ;
 
 \ 8.6.1.1160 DABS
 \ ( d -- ud )
-\ ud is the absolute value of d. 
+\ ud is the absolute value of d.
 \ Assuming 2-complement representation
 : DABS (S d -- ud )
   DUP SIGN-FILL DUP \ d sd
@@ -165,6 +165,10 @@ BASE !
   ELSE
     R> UT/
   THEN
+;
+
+: 2+! \ (S x1 x2 addr -- )
+  DUP >R 2@ D+ R> 2!
 ;
 
 REPORT-NEW-NAME !
