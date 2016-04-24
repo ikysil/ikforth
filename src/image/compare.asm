14552f113075c40110e4f640f43e783def28fc72
Illya Kysil <ikysil@ikysil.name>
2016-04-24 20:57:47 +0200

    Simplify code for comparizon operations by using template defined with macro.

--- src/image/compare.asm
+++ src/image/compare.asm
@@ -23,105 +23,76 @@
                 PUSHDS      F_TRUE
                 $NEXT
 
-;  <
-                $CODE       '<',$LE
+                MACRO       COMP_OP COND {
+                LOCAL       COMP_TRUE
                 POPDS       EBX
                 POPDS       EAX
                 CMP         EAX,EBX
                 MOV         EAX,F_TRUE
-                JL          SHORT LE_YES
+                J#COND      SHORT COMP_TRUE
                 MOV         EAX,F_FALSE
-LE_YES:
+COMP_TRUE:
                 PUSHDS      EAX
+                }
+
+                MACRO       COMPZ_OP COND {
+                LOCAL       COMPZ_TRUE
+                POPDS       EAX
+                OR          EAX,EAX
+                MOV         EAX,F_TRUE
+                J#COND      SHORT COMPZ_TRUE
+                MOV         EAX,F_FALSE
+COMPZ_TRUE:
+                PUSHDS      EAX
+                }
+
+;  <
+                $CODE       '<',$LE
+                COMP_OP     L
                 $NEXT
 
 ;  >
                 $CODE       '>',$GR
-                POPDS       EBX
-                POPDS       EAX
-                CMP         EAX,EBX
-                MOV         EAX,F_TRUE
-                JG          SHORT GR_YES
-                MOV         EAX,F_FALSE
-GR_YES:
-                PUSHDS      EAX
+                COMP_OP     G
                 $NEXT
 
 ;  6.1.0530 =
 ;  D: a b -- flag ( a = b )
                 $CODE       '=',$EQ
-                POPDS       EBX
-                POPDS       EAX
-                CMP         EAX,EBX
-                MOV         EAX,F_TRUE
-                JZ          SHORT EQ_YES
-                MOV         EAX,F_FALSE
-EQ_YES:
-                PUSHDS      EAX
+                COMP_OP     Z
                 $NEXT
 
 ;  6.2.0500 <>
 ;  Flag is true if and only if x1 is not bit-for-bit the same as x2.
 ;  D: x1 x2 -- flag
                 $CODE       '<>',$NOEQ
-                POPDS       EBX
-                POPDS       EAX
-                CMP         EAX,EBX
-                MOV         EAX,F_TRUE
-                JNZ         SHORT NOEQ_YES
-                MOV         EAX,F_FALSE
-NOEQ_YES:
-                PUSHDS      EAX
+                COMP_OP     NZ
                 $NEXT
 
 ;  6.1.0250 0<
 ;  D: a -- flag ( a < 0 )
                 $CODE       '0<',$ZEROLE,VEF_USUAL
-                POPDS       EAX
-                OR          EAX,EAX
-                MOV         EAX,F_TRUE
-                JL          SHORT ZEROLE_YES
-                MOV         EAX,F_FALSE
-ZEROLE_YES:
-                PUSHDS      EAX
+                COMPZ_OP    L
                 $NEXT
 
 ;  6.1.0270 0=
 ;  D: a -- flag ( a = 0 )
                 $CODE       '0=',$ZEROEQ,VEF_USUAL
-                POPDS       EAX
-                OR          EAX,EAX
-                MOV         EAX,F_TRUE
-                JZ          SHORT ZEROEQ_YES
-                MOV         EAX,F_FALSE
-ZEROEQ_YES:
-                PUSHDS      EAX
+                COMPZ_OP    Z
                 $NEXT
 
 ;  6.2.0260 0<>
 ;  Flag is true if and only if x is not equal to zero.
 ;  D: x -- flag
                 $CODE       '0<>',$ZERONOEQ,VEF_USUAL
-                POPDS       EAX
-                OR          EAX,EAX
-                MOV         EAX,F_TRUE
-                JNZ         SHORT ZERONOEQ_YES
-                MOV         EAX,F_FALSE
-ZERONOEQ_YES:
-                PUSHDS      EAX
+                COMPZ_OP    NZ
                 $NEXT
 
 ;  6.2.0280 0>
 ;  Flag is true if and only if n is greater than zero.
 ;  D: n -- flag
                 $CODE       '0>',$ZEROGR,VEF_USUAL
-                POPDS       EAX
-                OR          EAX,EAX
-                MOV         EAX,F_TRUE
-                JG          SHORT ZEROGR_YES
-                MOV         EAX,F_FALSE
-ZEROGR_YES:
-                PUSHDS      EAX
+                COMPZ_OP    G
                 $NEXT
 
 ;******************************************************************************
@@ -132,26 +103,12 @@ ZEROGR_YES:
 ;  Flag is true if and only if u1 is less than u2.
 ;  D: u1 u2 -- flag ( u1 < u2 )
                 $CODE       'U<',$ULE,VEF_USUAL
-                POPDS       EBX
-                POPDS       EAX
-                CMP         EAX,EBX
-                MOV         EAX,F_TRUE
-                JB          SHORT ULE_YES           ; jump if EAX < EBX
-                MOV         EAX,F_FALSE
-ULE_YES:
-                PUSHDS      EAX
+                COMP_OP     B
                 $NEXT
 
 ;  6.2.2350 U>
 ;  D: u1 u2 -- flag
 ;  flag is true if and only if u1 is greater than u2.
                 $CODE       'U>',$UGR,VEF_USUAL
-                POPDS       EBX
-                POPDS       EAX
-                CMP         EAX,EBX
-                MOV         EAX,F_TRUE
-                JA          SHORT UGR_YES           ; jump if EAX > EBX
-                MOV         EAX,F_FALSE
-UGR_YES:
-                PUSHDS      EAX
+                COMP_OP     A
                 $NEXT
