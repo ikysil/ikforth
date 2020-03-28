478a70cdc4371fe61f7ccf37395fd95e3e0a61c3
Illya Kysil <ikysil@ikysil.name>
2020-03-28 15:10:09 +0100

    Repair commands detection

--- docker/build-ci.sh
+++ docker/build-ci.sh
@@ -15,14 +15,17 @@ check_command() {
     docker_run "command -v $1 >/dev/null 2>&1"
 }
 
+skip() {
+    echo "$*"
+    exit 0
+}
+
 docker_run "echo -e '123\n ' | scons itc ansitest" || exit 1
 docker_run "echo -e '123\n ' | scons dtc ansitest" || exit 1
 docker_run "echo -e 'bye\n ' | scons fptest"       || exit 1
 
-check_command "wine" || \
-    ( echo ">>> WINE not installed, skipping..." && exit 0 )
-check_command "mingw32-g++" || \
-    ( echo ">>> Mingw32 not installed, skipping..." && exit 0 )
+check_command "wine32" || skip ">>> WINE not installed, skipping..."
+check_command "mingw32-g++" || skip ">>> Mingw32 not installed, skipping..."
 
 docker_run "scons itc term win32 all" || exit 1
 docker_run "scons dtc term win32 all" || exit 1
