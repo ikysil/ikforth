fb7541ed3eb50d19d42a9e9a325b2e9786d058a9
Illya Kysil <ikysil@ikysil.name>
2020-04-18 12:39:20 +0200

    Refactor docker files - use shared script to install FASM

---
+++ docker/install-fasm.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# keep going when something fails
+set +e
+set -o pipefail
+
+fasm_version=1.73.21
+workdir=`mktemp -d fasm.XXXXXXXXXX`
+
+cd ${workdir}
+
+wget http://flatassembler.net/fasm-${fasm_version}.tgz
+tar xf fasm-${fasm_version}.tgz
+mv $PWD/fasm /opt/fasm-${fasm_version}
+ln -s /opt/fasm-${fasm_version}/fasm /usr/local/bin/fasm
+
+cd /opt/fasm-${fasm_version}/tools/libc
+
+fasm listing.asm
+gcc -m32 -o ../../listing listing.o
+chmod +x ../../listing
+ln -s $PWD/../../listing /usr/local/bin/listing
