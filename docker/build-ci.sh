12907693f05af58adc75a9d38e767317334fe478
Illya Kysil <ikysil@ikysil.name>
2020-04-19 16:57:31 +0200

    Hide logs unless an error happens on CI

--- docker/build-ci.sh
+++ docker/build-ci.sh
@@ -20,16 +20,16 @@ skip() {
     exit 0
 }
 
-docker_run "echo -e '123\n ' | scons linux itc all ansitest" || exit 1
-docker_run "echo -e '123\n ' | scons linux dtc all ansitest" || exit 1
-docker_run "echo -e 'bye\n ' | scons linux fptest"           || exit 1
+docker_run "echo -e '123\n ' | docker/hide-logs.sh scons linux itc all ansitest" || exit 1
+docker_run "echo -e '123\n ' | docker/hide-logs.sh scons linux dtc all ansitest" || exit 1
+docker_run "echo -e 'bye\n ' | docker/hide-logs.sh scons linux fptest"           || exit 1
 
 check_command "wine32" || skip ">>> wine32 not installed, skipping..."
 check_command "mingw32-g++" || skip ">>> mingw32-g++ not installed, skipping..."
 check_command "mingw32-gcc" || skip ">>> mingw32-gcc not installed, skipping..."
 
-docker_run "echo -e '123\n ' | scons win32 itc term all ansitest" || exit 1
-docker_run "echo -e '123\n ' | scons win32 dtc term all ansitest" || exit 1
-docker_run "echo -e 'bye\n ' | scons win32 term fptest"           || exit 1
+docker_run "echo -e '123\n ' | docker/hide-logs.sh scons win32 itc term all ansitest" || exit 1
+docker_run "echo -e '123\n ' | docker/hide-logs.sh scons win32 dtc term all ansitest" || exit 1
+docker_run "echo -e 'bye\n ' | docker/hide-logs.sh scons win32 term fptest"           || exit 1
 
 docker_run "scons dist" || exit 1
