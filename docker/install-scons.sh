58598f5cd23410a19b9b178c316ed844680a3f5a
Illya Kysil <ikysil@ikysil.name>
2021-01-24 22:39:24 +0100

    (build): Change build system to use SCons 4.1.x and Python 3.6+ as result
    Repairs Build process is failing with SCons 4.x #57

--- docker/install-scons.sh
+++ docker/install-scons.sh
@@ -4,13 +4,12 @@
 set -o errexit
 set -o pipefail
 
-if [[ ! -f /usr/bin/python ]]; then
-    ln -s /usr/bin/python3 /usr/bin/python
+if [[ ! -f /usr/bin/python3 ]]; then
+    echo "Python 3 is required, but not found"
+    exit 1
 fi
 
-PIP="python -m pip"
+PIP="python3 -m pip"
 
 $PIP install --upgrade pip
-$PIP install --prefix=/opt/scons -r /opt/docker/python-requirements.txt
-
-ln -s /opt/scons/bin/scons /usr/local/bin/scons
+$PIP install -r /opt/docker/python-requirements.txt
